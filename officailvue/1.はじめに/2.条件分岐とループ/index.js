5dd6dccec5045dac8286233c7668cd1f759dfebe
田邊 悠人 <haruto_tanabe@en-japan.com>
2020-11-08 11:50:35 +0900

    test: 💍 Vue Study Until Lesson3

---
+++ officailvue/1.はじめに/2.条件分岐とループ/index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    seen: true
+  }
+})
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
---
+++ officailvue/3.算出プロパティとウォッチャ /1.算出プロパティ/index.js
@@ -0,0 +1,13 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 算出 getter 関数
+    reversedMessage: function () {
+      // `this` は vm インスタンスを指します
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
